25180e95ac8ab26fb6a14fb4e03852aa801afcfd
Ragulan <ragulan.gopalapillai@pagero.com>
2020-01-06 10:36:17 +0530

    search by name and author done

--- src/app/guery.service.ts
+++ src/app/guery.service.ts
@@ -17,6 +17,7 @@ export class QueryService {
 
     let sparqlQuery;
     if (authorName && bookName) {
+      sparqlQuery = this.getByBookAndAuthorQuery(bookName,authorName);
     } else if (authorName) {
       sparqlQuery = this.getByAuthorQuery(authorName);
 
@@ -24,6 +25,7 @@ export class QueryService {
       sparqlQuery = this.getByBookQuery(bookName);
     } else {
       console.log('both empty');
+      sparqlQuery = this.getQuery();
     }
 
     sparqlQuery = sparqlQuery + `\nLIMIT ${count}`;
@@ -66,6 +68,53 @@ export class QueryService {
       '{\n' +
       `?book ?title "${bookName}"@en .` +
       '\t?book wdt:P31 wd:Q571 .\n' +
+      '\t?book wdt:P50 ?author .\n' +
+      '\tOPTIONAL {\n' +
+      '\t\t?book wdt:P136 ?genre .\n' +
+      '\t\t?genre rdfs:label ?genre_label filter (lang(?genre_label) = "en").\n' +
+      '\t}\n' +
+      '\tOPTIONAL {\n' +
+      '\t\t?book wdt:P179 ?series .\n' +
+      '\t\t?series rdfs:label ?series_label filter (lang(?series_label) = "en").\n' +
+      '\t}\n' +
+      '\tOPTIONAL {\n' +
+      '\t\t?book wdt:P577 ?publicationDate .\n' +
+      '\t}\n' +
+      '\tSERVICE wikibase:label {\n' +
+      '\t\tbd:serviceParam wikibase:language "en" .\n' +
+      '\t}\n' +
+      '}';
+  }
+
+  private getByBookAndAuthorQuery(bookName: string,authorName: string) {
+    return ' SELECT ?book ?bookLabel ?authorLabel ?genre_label ?series_label ?publicationDate\n' +
+      'WHERE\n' +
+      '{\n' +
+      `\t?author ?label "${authorName.toString()}"@en .\n` +
+      `?book ?title "${bookName}"@en .` +
+      '\t?book wdt:P31 wd:Q571 .\n' +
+      '\t?book wdt:P50 ?author .\n' +
+      '\tOPTIONAL {\n' +
+      '\t\t?book wdt:P136 ?genre .\n' +
+      '\t\t?genre rdfs:label ?genre_label filter (lang(?genre_label) = "en").\n' +
+      '\t}\n' +
+      '\tOPTIONAL {\n' +
+      '\t\t?book wdt:P179 ?series .\n' +
+      '\t\t?series rdfs:label ?series_label filter (lang(?series_label) = "en").\n' +
+      '\t}\n' +
+      '\tOPTIONAL {\n' +
+      '\t\t?book wdt:P577 ?publicationDate .\n' +
+      '\t}\n' +
+      '\tSERVICE wikibase:label {\n' +
+      '\t\tbd:serviceParam wikibase:language "en" .\n' +
+      '\t}\n' +
+      '}';
+  }
+
+  private getQuery() {
+    return ' SELECT ?book ?bookLabel ?authorLabel ?genre_label ?series_label ?publicationDate\n' +
+      'WHERE\n' +
+      '{\n' +
       '\t?book wdt:P50 ?author .\n' +
       '\tOPTIONAL {\n' +
       '\t\t?book wdt:P136 ?genre .\n' +
